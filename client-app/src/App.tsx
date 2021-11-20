36d3f6bd79c607adba1569500b031bbb968a928b
CodeWithSB <shivab6863@gmail.com>
2021-11-20 00:47:54 -0500

    react + tailwindcss boilerplate

---
+++ client-app/src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+	return <h1 className="text-center text-4xl text-green-500 font-semibold"> Hello World!!! </h1>;
+}
+
+export default App;
---
+++ client-app/src/index.tsx
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(<div><App /></div>, document.getElementById("root"));
